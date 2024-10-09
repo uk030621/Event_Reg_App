6b51a57f5fc75fcabdfb247d5d11521e92f47789
uk030621 <123797901+uk030621@users.noreply.github.com>
2024-10-09 13:19:12 +0100

    Improved formatting

--- app/page.js
+++ app/page.js
@@ -37,6 +37,8 @@ export default function HomePage() {
         }
     };
 
+    const [loading, setLoading] = useState(true);
+
     const fetchUsers = async () => {
         try {
             const response = await fetch("/api/users");
@@ -44,6 +46,8 @@ export default function HomePage() {
             setUsers(data);
         } catch (error) {
             console.error("Error fetching users:", error);
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -105,11 +109,11 @@ export default function HomePage() {
         <div className={styles.border} style={{padding: "20px", maxWidth: "600px", margin: "0 auto", marginTop: "20px", borderRadius: "5px" }}>
             <h1 className="font-bold text-3xl mb-1">Thursday Club</h1>
             <h1 className="ml-3 font-bold text-xl mb-3">Event Registration</h1>
-            <p className="ml-3 text-sm">Venue: <span className="font-bold">TBA</span></p>
-            <p className="ml-3 text-sm ">Date: <span className="font-bold mr-4">TBA</span>Time: <span className="font-bold mr-4">TBA</span></p>
-            <Link className="ml-3 text-xs mt-2 cursor-pointer" href="https://thursday-club-blog.vercel.app/">Back to blog</Link>
+            <p className="ml-3 text-base">Venue: <span className="font-bold text-sm">TBA</span></p>
+            <p className="ml-3 text-base ">Date: <span className="font-bold mr-4 text-sm">TBA</span>Time: <span className="font-bold mr-4 text-sm">TBA</span></p>
+            <Link className="ml-3 text-sm mt-2 cursor-pointer" href="https://thursday-club-blog.vercel.app/">Back to blog</Link>
            
-            <p className="ml-3 font-bold text-sm mt-4">Attendee Registration</p>
+            <p className="ml-3 font-bold text-sm mt-3">Attendee Registration</p>
             <form className="flex flex-col ml-3" onSubmit={handleSubmit} style={{ marginBottom: "20px" }}>
                 <input
                     type="text"
@@ -120,8 +124,9 @@ export default function HomePage() {
                 />
                 <button className="bg-black text-white rounded-md w-fit" type="submit" style={{ padding: "6px 10px" }}>Submit</button>
             </form>
-
-            <h2 className="text-xl mt-10 mb-6 font-bold ml-3">Users List</h2>
+            <h2 className="text-base mt-6 mb-3 font-bold ml-3">Registered 
+                <span className="text-base ml-3 font-normal">{loading ? "(Loading...)" : `(${users.length} ${users.length === 1 ? 'attendee' : 'attendees'})`}</span>
+            </h2>
             <ul className="ml-3" style={{ listStyle: "none", padding: 0 }}>
                 {users.map((user) => (
                     <li key={user._id} style={{ marginBottom: "10px" }}>
@@ -131,7 +136,7 @@ export default function HomePage() {
                                     type="text"
                                     value={editingName}
                                     onChange={(e) => setEditingName(e.target.value)}
-                                    style={{ padding: "6px", width: "150px", marginRight: "10px"}}
+                                    className={styles.nameList}
                                 />
                                 <button className="text-white rounded-md" type="submit" style={{ padding: "2px 2px", marginRight: "5px" }}>
                                     👍
@@ -144,7 +149,7 @@ export default function HomePage() {
                         ) : (
                             <>
                             <div className="flex items-center justify-between">
-                                <span style={{ marginRight: "20px" }}>{user.name}</span>
+                                <span className="text-sm" style={{ marginRight: "20px" }}>{user.name}</span>
                                 <div className="flex items-center justify-end">
                                     <button className="text-black rounded-md mr-5" onClick={() => handleEdit(user)} style={{ padding: "2px 2px" }}>
                                         ✏️
